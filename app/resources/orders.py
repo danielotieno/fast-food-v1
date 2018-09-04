0a311a23435ce14358ad31f588115b9aefdf71b9
Daniel Otieno <danielotieno.ke@gmail.com>
2018-09-04 23:10:49 +0300

    [Chore: #160244710] Improve POST method code

--- app/resources/orders.py
+++ app/resources/orders.py
@@ -11,6 +11,9 @@ class Orders(Resource):
     """ Place a new Order method """
 
     def post(self, order_id):
+        if next(filter(lambda x: x['order_id'] == order_id, orders), None):
+            return { 'message': "The order with order id '{}' already exists.".format(order_id)}, 400
+
         data = request.get_json()
         order = {
             'order_id': order_id,
