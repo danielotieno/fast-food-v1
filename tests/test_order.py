b7ec03f0b10a15c1cc0e4e49694507828dc5a85e
Daniel Otieno <danielotieno.ke@gmail.com>
2018-09-05 18:18:45 +0300

    [Chore: Add TestBase]

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,7 +1,18 @@
-import pytest
+import unittest
+import os
 import json
-
+from flask_testing import TestCase
 from app import create_app
+
+
+class TestBase(TestCase):
+
+    def create_app(self):
+        """ Add Test configuration """
+        config_name = 'testing'
+        app = create_app(config_name)
+
+        return app
 
 
 from app.resources.orders import Order, Orders
@@ -12,3 +23,7 @@ GET_SINGLE_URL = '/api/v1/orders/1'
 GET_ALL_URL = '/api/v1/orders'
 DELETE_URL = '/api/v1/orders/1'
 MODIFY_URL = '/api/v1/orders/1'
+
+
+if __name__ == '__main__':
+    unittest.main()
