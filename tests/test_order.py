28a1cf2588ae7fd62deb17011f61c4d73c9f47eb
Daniel Otieno <danielotieno.ke@gmail.com>
2018-09-05 19:31:19 +0300

    [Test: #160244967] Add test for get all orders endpoint

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,8 +1,16 @@
 import unittest
-import os
 import json
 from flask_testing import TestCase
+
 from app import create_app
+
+from app.resources.orders import Order, Orders
+
+ADD_ENTRY_URL = '/api/v1/orders/1'
+GET_SINGLE_URL = '/api/v1/orders/1'
+GET_ALL_URL = '/api/v1/orders'
+DELETE_URL = '/api/v1/orders/1'
+MODIFY_URL = '/api/v1/orders/1'
 
 
 class TestBase(TestCase):
@@ -15,14 +23,15 @@ class TestBase(TestCase):
         return app
 
 
-from app.resources.orders import Order, Orders
+class TestOrders(TestBase):
 
-
-ADD_ENTRY_URL = '/api/v1/orders/1'
-GET_SINGLE_URL = '/api/v1/orders/1'
-GET_ALL_URL = '/api/v1/orders'
-DELETE_URL = '/api/v1/orders/1'
-MODIFY_URL = '/api/v1/orders/1'
+    def test_get_all_orders(self):
+        resource = self.client.get(
+            GET_ALL_URL, content_type='application/json')
+        data = json.loads(resource.data.decode())
+        print(data)
+        self.assertEqual(resource.content_type, 'application/json')
+        self.assertEqual(resource.status_code, 200)
 
 
 if __name__ == '__main__':
