e53a7a2992ffe5e0cba770d75e2e0af3f655bc35
Daniel Otieno <danielotieno.ke@gmail.com>
2018-09-07 03:17:55 +0300

    [Chore: Add test for user login]

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -9,7 +9,7 @@ LOGIN_URL = '/api/v1/auth/login'
 class TestAuth(EntryClass):
     """ Add tests for Auth """
 
-    def test_registration(self):
+    def test_user_registration(self):
         """ Test user registration works correcty """
         response = self.client.post(SIGNUP_URL,
                                     data=json.dumps(self.user_data), content_type='application/json')
@@ -18,7 +18,7 @@ class TestAuth(EntryClass):
         self.assertEqual(result["message"],
                          "registration successful, now login")
 
-    def test_wrong_registration(self):
+    def test_user_wrong_registration(self):
         """Test wrong registration when user doesn't fill fields"""
         response = self.client.post(SIGNUP_URL,
                                     data=json.dumps(
@@ -28,7 +28,7 @@ class TestAuth(EntryClass):
         result = json.loads(response.data.decode())
         self.assertEqual(result["message"], "All fields are required")
 
-    def test_cannot_register_twice(self):
+    def test_user_cannot_register_twice(self):
         """ Test User cannot register twice """
         self.client.post(SIGNUP_URL,
                          data=json.dumps(self.user_data), content_type='application/json')
@@ -61,7 +61,7 @@ class TestAuth(EntryClass):
                          "Username should be atleast 4 characters")
 
     def test_user_cannot_register_with_wrong_email_format(self):
-        """Test API cannot successfully register user if email is invalid(POST request)"""
+        """ Test user should not be able to register with invalid email """
         response = self.client.post(SIGNUP_URL,
                                     data=json.dumps(
                                         {'username': 'dannyb', 'email': 'danny@', 'password': 'pass254'}),
@@ -70,3 +70,24 @@ class TestAuth(EntryClass):
         result = json.loads(response.data.decode())
         self.assertEqual(
             result["message"], "Invalid email. Ensure email is of the form example@mail.com")
+
+    def test_user_cannot_register_with_invalid_username(self):
+        """ Test user should not be able to register with invalid username """
+        response = self.client.post(SIGNUP_URL,
+                                    data=json.dumps(
+                                        {'username': '#_danny', 'email': 'danny@gmail.com', 'password': 'pass12345'}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        result = json.loads(response.data.decode())
+        self.assertEqual(result["message"], "Invalid username")
+
+    def test_user_login(self):
+        """ Test registered user should be able to login """
+        self.test_user.save()
+        response = self.client.post(LOGIN_URL,
+                                    data=json.dumps(
+                                        {'username': 'dannyb', 'password': 'pass12345'}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.data.decode())
+        self.assertEqual(result["message"], "You are successfully logged in")
