fb2ee85922ba64fe6052ab3352c47c636159e62e
Daniel Otieno <danielotieno.ke@gmail.com>
2018-09-12 14:43:39 +0300

    [Chore: Test to get an user from the model]

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -5,6 +5,14 @@ from .entry import EntryClass
 class TestUserModel(EntryClass):
     """ Class for user model test cases """
 
+    def test_get_user(self):
+        """ Test to get an user  """
+        self.user1.save()
+        user = User.get(id=1)
+        self.assertIsInstance(user, User)
+        keys = sorted(list(user.view().keys()))
+        self.assertListEqual(keys, sorted(['username', 'email', 'id']))
+
     def test_can_save_user(self):
         """ Test to save user """
         user = self.user1.save()
