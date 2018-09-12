21bef5d7712b2a3d5a35e9591b6d20873526b0f0
Daniel Otieno <danielotieno.ke@gmail.com>
2018-09-12 12:52:03 +0300

    [Bug : Implement non-persistent data storage using data structures]

--- app/model.py
+++ app/model.py
@@ -96,22 +96,4 @@ def is_blank(var):
     return None
 
 
-orders = [
-
-    {
-        "order_id": 1,
-        "name": "Daniel Otieno",
-        "type": "Cheese Burger",
-        "price": 686.00,
-        "address": "Kileleshwa"
-    },
-
-    {
-        "order_id": 2,
-        "name": "Cash Camerine",
-        "type": "Chips Masala",
-        "price": 400.00,
-        "address": "Parklands"
-    }
-
-]
+orders = []
