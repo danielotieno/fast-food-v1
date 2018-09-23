79ac5da304b698ba66c77af95711aa4db0414c8b
Daniel Otieno <danielotieno.ke@gmail.com>
2018-09-23 09:18:42 +0300

    [Chore: Add a create method for order]

--- app/v1/models/order.py
+++ app/v1/models/order.py
@@ -23,3 +23,23 @@ class Order():
         if date != date.today():
             return False
         return True
+
+    def create(self, user_id, food_id, address, order_date, orderedby):
+        """A method for placing an order """
+
+        # empty dictionary to hold order details created
+        self.order_details = {}
+
+        # validate order date
+        if self.validate_date(order_date):
+            return "order can only have a future date"
+        else:
+            self.order_details['user_id'] == user_id
+            self.order_details['food_id'] == food_id
+            self.order_details['address'] == address
+            self.order_details['order_date'] == order_date
+            self.order_details['date_created'] = date.today().isoformat()
+            self.order_details['orderedby'] == orderedby
+            self.order_details['id'] = uuid.uuid1()
+            self.orders.append(self.order_details)
+            return "Successfull order created"
