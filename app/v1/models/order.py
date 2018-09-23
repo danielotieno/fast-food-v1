cd562bbbdbe48eab9fc6296d31b866e62977f606
Daniel Otieno <danielotieno.ke@gmail.com>
2018-09-23 17:06:47 +0300

    [Chore: Update order model details]

--- app/v1/models/order.py
+++ app/v1/models/order.py
@@ -4,18 +4,18 @@ It also create data structure to store order data
 
 """
 
-# Local imports
+
+from flask import jsonify
 import re
 from datetime import date, datetime
 import uuid
 
+""" Data structure to hold orders """
+orders = []
+
 
 class Order():
     """ A class to handle actions related to orders """
-
-    def __init__(self):
-        """ Data structure to hold orders """
-        self.orders = []
 
     def validate_date(self, order_date):
         """ Check if the given date is not the current date """
@@ -24,69 +24,64 @@ class Order():
             return False
         return True
 
-    def create(self, user_id, food_id, address, order_date, orderedby):
+    def create(self, orderedby, address, order_date, total_cost):
         """A method for placing an order """
-
-        # empty dictionary to hold order details created
-        self.order_details = {}
 
         # validate order date
         if self.validate_date(order_date):
-            return "order can only have a future date"
+            return "order can only have a present date"
         else:
-            self.order_details['user_id'] == user_id
-            self.order_details['food_id'] == food_id
-            self.order_details['address'] == address
-            self.order_details['order_date'] == order_date
-            self.order_details['date_created'] = date.today().isoformat()
-            self.order_details['orderedby'] == orderedby
-            self.order_details['id'] = uuid.uuid1()
-            self.orders.append(self.order_details)
+            self.orderedby = orderedby
+            self.address = address
+            self.order_date = order_date
+            self.date_created = date.today().isoformat()
+            self.order_id = uuid.uuid1()
+            self.total_cost = total_cost
             return "Successfull order created"
 
     def view_all_orders(self):
         """ A method to return a list of all orders """
-        return self.orders
+        return jsonify({
+            "message": "Successful.",
+            "Orders": orders}), 200
 
     def filter_by_orderedby(self, username):
         """ Filter order by a particular user """
         new_orders = [
-            order for order in self.orders if order['orderedby'] == username]
+            order for order in orders if order['orderedby'] == username]
         return new_orders
 
     def get_order_by_id(self, order_id):
         """ A method to get order by id """
-        for order in self.orders:
-            if order['id'] == order_id:
+        for order in orders:
+            if order.id == order_id:
                 return order
         return False
 
-    def update(self, order_id, user_id, food_id, address, order_date, orderedby):
+    def update(self, order_id, orderedby, address, order_date, total_cost):
         """ Update a specific order with a given id """
-        for order in self.orders:
-            if order['id'] == order_id:
-                self.orders.remove(order)
+        for order in orders:
+
+            if order.id == order_id:
+                orders.remove(order)
                 if self.validate_date(order_date):
                     return "Order can only have the current date"
                 else:
-                    self.order_details['user_id'] == user_id
-                    self.order_details['food_id'] == food_id
-                    self.order_details['address'] == address
-                    self.order_details['order_date'] == order_date
-                    self.order_details['date_created'] = date.today(
-                    ).isoformat()
-                    self.order_details['orderedby'] == orderedby
-                    order['id'] = order_id
-                    self.orders.append(self.order_details)
+                    self.orderedby = orderedby
+                    self.address = address
+                    self.order_date = order_date
+                    self.date_created = date.today().isoformat()
+                    self.order_id = uuid.uuid1()
+                    self.total_cost = total_cost
                     return "Successfull order updated"
         else:
             return "No order with such id"
 
     def delete(self, order_id):
         """ A method to delete a specific order from a list """
-        for order in self.orders:
-            if order['id'] == order_id:
-                self.orders.remove(order)
+        for order in orders:
+            if order.id == order_id:
+                orders.remove(order)
                 return "Successfull order deleted"
         else:
             return "Order not found"
