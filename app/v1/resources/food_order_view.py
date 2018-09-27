f1189443dfe084b440337fe495f0b19d7d19cd70
Daniel Otieno <danielotieno.ke@gmail.com>
2018-09-27 14:12:23 +0300

    [Chore: update food order view]

--- app/v1/resources/food_order_view.py
+++ app/v1/resources/food_order_view.py
@@ -1,31 +1,46 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, jsonify
 from flask_restful import Resource
 
 from app.v1.models.food_order import orders, FoodOrder
 from app.v1.models.food_order_details import food_order_details, FoodOrderDetail
 from app.v1.models.food_item import FoodItem
+from app.v1.models.user import User
 
 
 class FoodOrderView(Resource):
 
     def post(self):
         """ Place a new Order method """
+        auth_header = request.headers.get('Authorization')
         order_data = request.get_json()
-        username = 'Bucky'
-        order_details_data = order_data['oderDetails']
+        if auth_header:
+            access_token = auth_header.split(" ")[1]
+            if access_token:
+                res = User.decode_auth_token(access_token,
+                                             os.getenv('SECRET_KEY'))
+                if 'login' in res:
+                    return jsonify({"message": res})
+                email = res
+                order_details_data = order_data['oderDetails']
 
-        order = FoodOrder(username)
-        order_id = order.id
+                order = FoodOrder(email)
+                print("who placed the order", order.ordered_by)
+                order_id = order.id
 
-        # loop over orderData and create order details
-        total_cost = 0
-        for data in order_details_data:
-            price = FoodItem.get_price_by_name(data['food_name'])
-            count = data['count']
-            cost = price * count
-            order_detail = FoodOrderDetail(order_id, data['food_item'], count,
-                                           cost)
-            food_order_details.append(order_detail)
-            total_cost += cost
-        order.set_total_cost(total_cost)
-        orders.append(order)
+                # loop over orderData and create order details
+                total_cost = 0
+                for data in order_details_data:
+                    price = FoodItem.get_price_by_name(data['food_item'])
+                    count = data['count']
+                    cost = price * count
+                    order_detail = FoodOrderDetail(order_id, data['food_item'], count,
+                                                   cost)
+                    food_order_details.append(order_detail)
+                    total_cost += cost
+                order.set_total_cost(total_cost)
+                print(" total cost", order.total_cost)
+                orders.append(order)
+                return jsonify({"message": "order placed successful"})
+            return jsonify({"message": "please login or register to continue"})
+        return jsonify({"message": "please login or register to continue"})
